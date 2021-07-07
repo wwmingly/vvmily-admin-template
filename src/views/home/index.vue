21b29514f08aea63bc2c52a71f23258b0d68c74e
wwmingly <2991374415@qq.com>
2021-07-07 15:36:14 +0800

    feat: home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,12 @@
 <template>
-  <div>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">{{ pathRoute }}</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger" @click="handleDelete">危险按钮</el-button>
-    </el-row>
+  <div class="home">
+    {{ pathRoute }}
   </div>
 </template>
 <script>
 export default {
   name: 'Home',
+  components: {},
   data() {
     return {}
   },
@@ -21,14 +15,8 @@ export default {
       return this.$route.path
     }
   },
-  methods: {
-    handleDelete() {
-      this.$notify({
-        title: '成功',
-        message: '这是一条成功的提示消息',
-        type: 'success'
-      })
-    }
-  }
+  created() {},
+  methods: {}
 }
 </script>
+<style lang="scss" scoped></style>
